b3c0fffa329e4fc346f6c072efa41af3a127d647
Koji Tanaka <kj.tanaka@gmail.com>
2014-08-18 08:12:43 -0400

    update attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,6 @@
 default['openmpi']['version'] = "1.8.1"
-default['openmpi']['download_url'] = "http://www.open-mpi.org/software/ompi/v1.8/downloads/openmpi-#{node['openmpi']['version']}.tar.bz2"
+major, minor, tiny = node['openmpi']['version']
+default['openmpi']['download_url'] = "http://www.open-mpi.org/software/ompi/v#{major}.#{minor}/downloads/openmpi-#{node['openmpi']['version']}.tar.bz2"
 default['openmpi']['download_dir'] = "/root/source"
 default['openmpi']['install_dir'] = "/opt"
 
